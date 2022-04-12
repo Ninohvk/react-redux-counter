d7d75156262e94b0f25e5ea3862865dbaba44f76
Yanina Hernandez <yanina.hernandez.vega@everis.com>
2022-04-12 18:28:32 -0400

    counter redux connection

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-export const Counter = () => {
+export const Counter = (props) => {
   return (
     <>
       <h1>Counter</h1>
       <p>
-        Current value: <span id="counter">0</span>
+        Current value: <span id="counter">{props.counter}</span>
       </p>
-      <button id="increment">+</button>
-      <button id="decrementS">-</button>
+      <button onClick={props.increment}>+</button>
+      <button onClick={props.decrement}>-</button>
     </>
   );
 };
